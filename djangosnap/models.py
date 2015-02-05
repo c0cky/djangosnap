010eb5158d6f520d232b3630e6f17951ca1eefa5
c0cky <godboutc@email.sc.edu>
2015-02-04 22:24:07 -0500

    initial

--- djangosnap/models.py
+++ djangosnap/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+def get_media_path(instance, filename):
+    return os.path.join('media', str(instance.id), filename)
+
+class Media(models.Model):
+    mediafile = models.FileField(upload_to='media/%Y/%m/%d')
+
