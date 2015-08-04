c9bceb4ee9ddc19271c050e8766e6fac9190d937
Camron Godbout <camron248@gmail.com>
2015-08-04 10:24:49 -0400

    design and color change
    *major ux updates

--- djangosnap/static/djangosnap/watchScript.js
+++ djangosnap/static/djangosnap/watchScript.js
@@ -1,48 +1,119 @@
 $( document ).ready(function () {
     console.log(mediaURLS);
-    for (var i = 0; i < mediaURLS.length; i++) {
-      if (isVid(mediaURLS[i])) {
-        $('.media-slider').append("<div><video src='" + mediaURLS[i] + "'" + "></video></div>");
-      } else {
-        $('.media-slider').append("<div><img src='" + mediaURLS[i] + "'" + "></img></div>");
-      }
-      
-    }
-
-    $('.media-slider').slick({
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-});
 });
 
+// console.log(mediaURLS);
+    var currentMediaIndex = 0;
+    var currentMediaURL = "";
+    if (mediaURLS.length > 0)
+    {
+        currentMediaURL = mediaURLS[currentMediaIndex];
+        if (mediaURLS.length > 1)
+        {
+            $('#next' ).css("visibility","visible");
+        }
+        loadMedia();
 
-     function isVid(url)
+
+    }
+
+     function previousMedia()
      {
-         var extension = url.substr(url.length - 3);
-         if (extension === 'mp3')
+         var temp = currentMediaIndex - 1;
+         if (temp >= 0 )
          {
-             return true;
+             if (temp == 0)
+             {
+                 $('#previous').css("visibility","hidden");
+             }
+             currentMediaIndex--;
+             loadMedia();
          }
-         return false;
+
      }
+ 
+     function nextMedia()
+     {
+         var temp = currentMediaIndex + 1;
+         if (temp < mediaURLS.length)
+         {
+             if (temp == mediaURLS.length -1)
+             {
+                 $('#next').css("visibility","hidden");
+             }
+             if (currentMediaIndex == 0)
+             {
+                 $('#previous').css("visibility","visible")
+             }
+             currentMediaIndex++;
+             loadMedia(currentMediaIndex);
+         }
+
+     }
+
+    function loadMedia()
+    {
+        var vid = isVid(mediaURLS[currentMediaIndex]);
+        if (vid)
+        {
+            loadVideo();
+        }
+        else
+        {
+            loadImage();
+        }
+    }
+
+     function loadVideo()
+     {
+         closeOutImage();
+         var width = $('#innercover').width();
+         var height = $('#inercover').height();
+         $('#vid').css("visibility","visible");
+         $('#vid').width(width);
+         $('#vid').height(height);
+         $('#vid').attr('src', mediaURLS[currentMediaIndex]);
+         $('#vid')[0].load();
+     }
+
+    function loadImage()
+    {
+        closeOutVideo();
+        var width = $('#innercover').width();
+        var height = $('#inercover').height();
+        $('#im').css("visibility","visible");
+        $('#im').width(width);
+        $('#im').height(height);
+        $('#im').attr('src', mediaURLS[currentMediaIndex]);
+
+    }
+
+    function closeOutImage()
+    {
+        $('#im').css("visibility","hidden");
+        $('#im').width(0);
+        $('#im').height(0);
+    }
+
+    function closeOutVideo()
+    {
+        $('#vid').css("visibility","hidden");
+        $('#vid').width(0);
+        $('#vid').height(0);
+    }
+$("#vid").on("swipeleft",function(){
+  previousMedia();
+});
+$("#vid").on("swiperight",function(){
+  nextMedia();
+});
+
+function isVid(url)
+{
+  var extension = url.substr(url.length - 3);
+  if (extension === 'mp3')
+  {
+    return true;
+  }
+  return false;
+}
