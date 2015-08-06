a26c2cf384e7a64668eb47efeca3d5685355fef1
Camron Godbout <camron248@gmail.com>
2015-08-06 10:52:41 -0400

    updates to models for migrations
    *because django 1.8 & django 1.7 :( WHY

--- djangosnap/models.py
+++ djangosnap/models.py
@@ -4,24 +4,38 @@ from django.core.exceptions import ValidationError
 # Create your models here.
 from django.contrib.auth.models import User
 from tastypie.models import create_api_key
+from django.utils.deconstruct import deconstructible
 
 models.signals.post_save.connect(create_api_key, sender=User)
 
 def get_media_path(instance, filename):
     return os.path.join('media', str(instance.id), filename)
 
-class Media(models.Model):
-    def validate_media(fieldfile_obj):
-        filesize = fieldfile_obj.file.size
+@deconstructible
+class PathAndValidate(object):
+    def __init__(self, sub_path):
+        self.path = sub_path
+
+    def __call__(self, instance, filename):
+        filesize = instance.file.size
         megabyte_limit = 5.0
         if filesize > megabyte_limit*1024*1024:
             raise ValidationError("Max file size is %sMB" % str(megabyte_limit))
-        ext = os.path.splitext(fieldfile_obj.name)[1]
+        ext = os.path.splitext(instance.name)[1]
         valid_extensions = ['.mp4','.png','.jpg']
         if not ext in valid_extensions:
             raise ValidationError(u'File not supported!')
+        # ext = filename.split('.')[-1]
+        # # set filename as random string
+        # filename = '{}.{}'.format(uuid4().hex, ext)
+        # return the whole path to the file
+        return os.path.join(self.path, filename)
 
-    mediafile = models.FileField(upload_to='djangosnap/%Y/%m/%d', validators=[validate_media])
+path_and_validate = PathAndValidate("/djangosnap/%Y/%m/%d/")
+
+
+class Media(models.Model):
+    mediafile = models.FileField(upload_to=path_and_validate)
     approved = models.BooleanField(default=False)
     up_vote = models.IntegerField(blank=True, default=0, null=True)
     down_vote = models.IntegerField(blank=True, default=0, null=True)
