cceb79b7e9198c1a467e1a7268ffe10b87e24cd9
Camron Godbout <camron248@gmail.com>
2015-08-12 01:17:04 -0400

    created tagalong_comments
    *added tagalong_comments
    *added up vote & down vote for custom comments
    *added to settings.py for COMMENTS_APP

---
+++ tagalong_comments/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django_comments.forms import CommentForm
+from tagalong_comments.models import TagalongComment
+
+class TagalongCommentForm(CommentForm):
+    up_vote = forms.IntegerField()
+    down_vote = forms.IntegerField()
+
+    def get_comment_model(self):
+        # Use our custom comment model instead of the built-in one.
+        return TagalongComment
+
+    def get_comment_create_data(self):
+        # Use the data of the superclass, and add in the title field
+        data = super(TagalongCommentForm, self).get_comment_create_data()
+        data['up_vote'] = self.cleaned_data['up_vote']
+        data['down_vote'] = self.cleaned_data['down_vote']
+        return data
